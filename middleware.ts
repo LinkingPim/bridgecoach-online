e089976c4072f89dce614246809073ae65f376ca
LinkingPim <pimfrikken@gmail.com>
2026-03-24 21:52:47 +0100

    middleware met inhoud

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization');
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
+
+    if (user === 'bridgecoach' && pwd === process.env.SITE_PASSWORD) {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse('Toegang vereist', {
+    status: 401,
+    headers: { 'WWW-Authenticate': 'Basic realm="BridgeCoach"' },
+  });
+}
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+};
